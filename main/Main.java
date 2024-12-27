6781dcf012e8e4c10be29d24383802cf2b72028f
111216032 <s111216032@stu.ntue.edu.tw>
2024-12-28 02:57:10 +0800

    Update Main.java
    加了一些註解

--- main/Main.java
+++ main/Main.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame();
+        //設定
         window.setResizable(false);
         window.setTitle("Treasure Hunting RPG");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -12,10 +13,11 @@ public class Main {
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
 
-        window.pack();
+        window.pack();// 根據其內容的首選大小調整視窗的大小，不用手動
 
-        window.setLocationRelativeTo(null);
+        window.setLocationRelativeTo(null);//設定視窗和指定component的相對位置，null放中間
         window.setVisible(true);
 
+        gamePanel.startGameThread();
     }
 }
